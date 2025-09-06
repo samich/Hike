a184729bdc1c8205488833e91f36bda55dc23dda
Sami <itssami@gmail.com>
2025-09-05 23:33:55 -0500

    Settings View and CustomListRowView

---
+++ Hike/Component/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  Hike
+//
+//  Created by Sami on 9/5/25.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
